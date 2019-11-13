b287e66e3958b6b7d8a654e0018c5d18a9c19745
Stela Capsa <stela.capsa@gmail.com>
2019-11-13 17:35:51 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,20 @@
 
 
 def countdown(number)
-  number = 1
-  while number < 10
-  puts "#{number} seconds! "
-  number -= 1
-  puts "HAPPY NEW YEAR!"
+  
+  while number >= 1
+   puts "#{number} SECOND(S)!"
+   number -= 1
+  end
+  
+  return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(sleep)
-time = Time.now
-sleep 10.seconds until Time.now > time + 10.seconds 
-end
+def countdown_with_sleep(time)
+  while time >= 1 
+  puts "#{time} SECOND(S)!"
+   sleep 5
+   time -= 1
+ end
+ return "sleep"
+ end
