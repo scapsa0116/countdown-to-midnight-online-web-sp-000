43c883a95e73cedd2bb1f8e19da0fc28cd920fa0
Stela Capsa <stela.capsa@gmail.com>
2019-11-13 16:34:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,11 +2,12 @@
 
 def countdown(number)
   while number < 10
-  puts "#{number} seconds! "
-  number -= 1
   
-  puts "HAPPY NEW YEAR!"
-end
+    puts "#{number} seconds! "
+    number -= 1
+    
+    puts "HAPPY NEW YEAR!"
+ end
 end
 
 def countdown_with_sleep(sleep)
